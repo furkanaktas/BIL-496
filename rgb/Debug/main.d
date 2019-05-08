05b6b79b1e54532c08e8ca65b662ba680cceb92d
Furkan Aktaş <f.aktas96@hotmail.com>
2019-05-08 15:58:03 +0300

    Add files via upload

---
+++ rgb/Debug/main.d
@@ -0,0 +1,75 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/string.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/math.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_defs.h
+main.obj: C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_limits.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_timer.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_nvic.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/utils/cmdline.h
+main.obj: ../LCD.h
+main.obj: C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/TM4C123GH6PM.h
+
+../main.c: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/string.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/math.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_defs.h: 
+C:/Program\ Files/ti/ccs830/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_limits.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_timer.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/rom.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/hw_nvic.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/utils/cmdline.h: 
+../LCD.h: 
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/inc/TM4C123GH6PM.h: 
---
+++ rgb/Debug/subdir_vars.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+LIB_SRCS += \
+C:/Program\ Files/ti/ccs830/TivaWare_C_Series-2.1.4.178/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../LCD.c \
+../main.c \
+../tm4c123gh6pm_startup_ccs.c 
+
+C_DEPS += \
+./LCD.d \
+./main.d \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./LCD.obj \
+./main.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"LCD.obj" \
+"main.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"LCD.d" \
+"main.d" \
+"tm4c123gh6pm_startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../LCD.c" \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
